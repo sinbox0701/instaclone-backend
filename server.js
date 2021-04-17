23baae91a67c3d7f23b3ccf3380f418f4f83ecca
sinbox0701 <psh090953@gmail.com>
2021-04-17 13:24:36 +0900

    Ejecting Server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
-import {ApolloServer} from "apollo-server";
+import express from "express";
+import logger from "morgan";
+import {ApolloServer} from "apollo-server-express";
 import {typeDefs,resolvers} from "./schema";
 import { getUser } from "./User/User.utils";
 
@@ -15,4 +17,12 @@ const server = new ApolloServer({
 
 const PORT = process.env.PORT
 
-server.listen().then(() => console.log(`Server is running on http://localhost:${PORT}/`));
+const app = express();
+//server로 Express를 사용
+app.use(logger("tiny"));
+//Server의 Resuqet를 볼수있는 Middleware
+server.applyMiddleware({app});
+//apollo server(graphql)은 우리의 express 서버에서 동작
+app.listen({port:PORT},()=>{
+    console.log(`Server is running on http://localhost:${PORT}/graphql`);
+});
