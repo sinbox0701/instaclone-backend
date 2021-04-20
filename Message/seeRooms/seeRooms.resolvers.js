4efd2afea523c9df31fe1a981f771f39eb7895c7
sinbox0701 <psh090953@gmail.com>
2021-04-20 16:25:28 +0900

    seeRooms

---
+++ Message/seeRooms/seeRooms.resolvers.js
@@ -0,0 +1,18 @@
+import client from "../../client";
+import { protectedResolver } from "../../User/User.utils";
+
+export default {
+  Query: {
+    seeRooms: protectedResolver(async (_, __, { loggedInUser }) =>
+      client.room.findMany({
+        where: {
+          users: {
+            some: {
+              id: loggedInUser.id,
+            },
+          },
+        },
+      })
+    ),
+  },
+};
